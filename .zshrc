66b4a7125ae8412b6a52dfc7cc9083053160ee8f
KlemensMaentele <klemens.maentele@gmail.com>
2022-03-26 17:21:11 +0100

    added zsh as standart shell

---
+++ .zshrc
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Lines configured by zsh-newuser-install
+autoload -U colors && colors	# Load colors
+PS1="%B%{$fg[blue]%}[%{$fg[white]%}%n%{$fg[white]%}%~%{$fg[blue]%}]%{$reset_color%}$%b "
+setopt autocd		# Automatically cd into typed directory.
+stty stop undef		# Disable ctrl-s to freeze terminal.
+
+HISTFILE=~/.config/zsh/.histfile
+HISTSIZE=1000000000000
+SAVEHIST=1000000000000
+
+bindkey -e
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+autoload -U compinit
+zstyle ':completion:*' menu select
+zmodload zsh/complist
+compinit
+_comp_options+=(globdots)
+
+#Aliases
+alias p="sudo pacman"
+alias ss="sudo systemctl"
+alias v="nvim"
+alias sv="sudo nvim"
+alias ls="ls -hN --color=auto"
+alias grep="grep --color=auto"
+
+
+export PATH="$PATH:$HOME/.local/bin/"
+
+# End of lines added by compinstallsource
+
+source ~/.config/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+eval $(thefuck --alias)
+source ~/.config/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ echo "Done"
 
 # Stuff you need
 installpkg "git"
-
+installpkg "python-pip"
 # setup folders
 mkdir ~/.local/share -p
 mkdir ~/.config/ -p
@@ -20,14 +20,14 @@ mkdir ~/.config/ -p
 # Install yay
 echo -n "Installing yay aur helper......"
 cd ~/.local/share
-git clone https://aur.archlinux.org/yay.git
+git clone https://aur.archlinux.org/yay.git >/dev/null 2>&1
 cd ./yay
 makepkg -si --needed --noconfirm >/dev/null 2>&1
 echo "Done"
 
 # Graphical shit intel drivers
 echo -n "Installing xorg stuff......"
-sudo pacman --noconfirm --needed -S mesa libx11 xorg-server xorg-xinit libxrandr libxinerama libxft xorg-xrdb xf86-video-intel xorg-xrandr xorg-xclipboard unclutter >/dev/null 2>&1
+sudo pacman --noconfirm --needed -S mesa libx11 xorg-server xorg-xinit libxrandr libxinerama libxft xorg-xrdb xf86-video-intel xorg-xrandr xorg-xclipboard unclutter xorg-xbacklight >/dev/null 2>&1
 echo "Done"
 
 # Font
@@ -45,7 +45,7 @@ echo "Done"
 echo -n "Installing DWM......"
 sudo pacman --noconfirm --needed -S xorg-xsetroot imlib2 >/dev/null 2>&1
 cd ~/.config
-git clone https://github.com/KlemensMaentele/chadwm --depth 1
+git clone https://github.com/KlemensMaentele/chadwm --depth 1 >/dev/null 2>&1
 cd ~/.config/chadwm/chadwm
 sudo make install >/dev/null 2>&1
 mkdir -p ~/.local/share/fonts
@@ -54,14 +54,15 @@ echo "Done"
 
 # Autostart
 chmod +x ~/.config/chadwm/scripts/bar.sh ~/.config/chadwm/scripts/autostart
-cp ~/dotfiles/.bash_profile ~/.bash_profile
-cp ~/dotfiles/.xinitrc ~/.xinitrc
+cp ~/dotfiles/.zshrc ~/
+cp ~/dotfiles/.xinitrc ~/
+cp ~/dotfiles/.zprofile ~/
 
 
 # ST terminal
 echo -n "Installing the terminal......"
 cd ~/.config/
-git clone https://github.com/KlemensMaentele/st
+git clone https://github.com/KlemensMaentele/st >/dev/null 2>&1
 cd st
 sudo make install >/dev/null 2>&1
 #sudo apt install ncurses-term # support for 256color on ssh
@@ -72,16 +73,26 @@ echo -n "Installing Browser......"
 installpkgaur "brave-bin"
 echo "Done"
 
-echo -n "Installing programs (mpv, ranger, tlp, ueberzug)....."
-sudo pacman --noconfirm --needed -S mpv ranger neofetch tlp ueberzug >/dev/null 2>&1
-sudo systemctl enable tlp --now
+echo -n "Installing programs....."
+sudo pacman --noconfirm --needed -S sxiv mpv ranger neofetch tlp ueberzug feh >/dev/null 2>&1
+sudo systemctl enable tlp --now >/dev/null 2>&1
 echo "Done"
 
 # nvchad
 echo -n "Installing neovim......"
 installpkg "neovim"
 mkdir -p ~/.config/nvim
-cd ~/.config/nvim/
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 ~/.config/nvim >/dev/null 2>&1
 nvim +'hi NormalFloat guibg=#1e222a' +PackerSync
 echo "Done"
+
+# zsh todo add zshrc and zprofile to repo and copy them over , use zsh as standart shell
+echo "Installing zshell......"
+installpkg "zsh"
+pip3 install thefuck --user >/dev/null 2>&1
+mkdir ~/.config/zsh/
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/zsh-autosuggestions >/dev/null 2>$1
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/zsh-syntax-highlighting >/dev/null 2>$1
+chsh -s /usr/bin/zsh $user # setting zsh as standart shell
+
+# add source line to zshrc
