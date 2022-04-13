7f6bae26881462dd0cac2cef54a13dec04b5ff6b
KlemensMaentele <klemens.maentele@gmail.com>
2022-04-13 13:50:01 +0200

    Added ranger scripts and icon plugins

---
+++ configs/ranger/plugins/commands.py
@@ -0,0 +1,17 @@
+from ranger.api.commands import *
+import subprocess
+
+class update_wallpaper(Command):
+    """
+    :update_wallpaper
+    Create systemlink to ~/Pictures/wall/wall.png and update
+    """
+    def execute(self):
+        subprocess.check_call(['updatewall', self.fm.thisfile.path ])
+class trash(Command):
+    """
+    :trash
+    Use trash script
+    """
+    def execute(self):
+        subprocess.check_call(['trash', self.fm.thisfile.path ])
